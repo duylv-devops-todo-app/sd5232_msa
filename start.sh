b0a5b5e5aa42db50e7f0c3663fba5e726a554acd
Duy Le Van <duy.levan@nashtechglobal.com>
2024-12-23 16:12:02 +0700

    edit frontend.yaml

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kubectl config set-context --current --namespace eks-ns-1
+
+kubectl apply -f mongodb.yaml
+kubectl apply -f backend.yaml
+kubectl apply -f frontend.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.1/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ingress.yml
+
+kubectl get ingress -o wide
